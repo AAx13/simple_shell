c3a112f40068f3d76f81045b0b65383736cfe0d3
DamianAli <damian.ali33@gmail.com>
2017-05-09 16:16:38 -0700

    testing exit.c against hbtn checks.

--- exit.c
+++ exit.c
@@ -14,11 +14,11 @@ void exit_b(char **tokens, char *line)
 	int status;
 
 	status = 0;
-	if (tokens[1] != NULL)
+	/*if (tokens[1] != NULL)
 	{
 		status = _atoi(tokens[1]);
 	}
-	putstr("[ Exiting ]\n");
+	putstr("[ Exiting ]\n");*/
 	free(line);
 	free(tokens);
 	exit(status);
